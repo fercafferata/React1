0c1bd3441a4f6014ff0b6c5f8214060a6109c0ee
FerCafferata <fercafferata@outlook.com>
2020-10-24 16:44:06 -0300

    app terminada

--- app-clima/src/App.js
+++ app-clima/src/App.js
@@ -1,33 +1,69 @@
-import React, { Fragment } from 'react';
-import Formulario from './Componentes/Formulario';
-import Header from './Componentes/Header'
+import React, { Fragment, useEffect, useState } from 'react';
+import Formulario from './components/Formulario';
+import Header from './components/Header';
+import Clima from './components/Clima'
 
 
 function App() {
-  return (
-    <Fragment>
-      <Header titulo='Clima App'/>
 
-      <div className="contenedor-form">
-        <div className="container">
-          <div className="row">
+    //state del formulario
+    const [busqueda, guardarBusqueda] = useState({
+      ciudad:'',
+      pais:''
+    })
 
-            <div className="col m6 s12">
-              <Formulario/>
+    const [consultar, guardarConsulta] = useState(false)
+
+    const [resultado, guardarResultado] = useState({})
+
+    const {ciudad, pais} = busqueda
+
+
+
+    useEffect( () => {
+      // consulta de API
+      const consultarAPI = async() => {
+        if (consultar) {
+          const appId = 'cd26d0807b3428b974f1657ca3329966'
+          const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`
+        
+          const respuesta = await fetch(url);
+          const resultado = await respuesta.json()
+
+          guardarResultado(resultado);
+          guardarConsulta(false)
+        }
+      }
+      consultarAPI()
+    }, [consultar]) //las dependencia se pasan como un arreglo vacio
+
+
+    return (
+      <Fragment>
+        <Header titulo='Clima React App'/>
+
+        <div className="contenedor-form">
+          <div className="container">
+            <div className="row">
+
+              <div className="col m6 s12">
+                <Formulario
+                  busqueda={busqueda}
+                  guardarBusqueda={guardarBusqueda}
+                  guardarConsulta={guardarConsulta}
+                />
+              </div>
+
+              <div className="col m6 s12">
+                <Clima
+                  resultado={resultado}
+                />
+              </div>
             </div>
-
-            <div className="col m6 s12">
-              
-            </div>
-
           </div>
-
-          
-          
         </div>
-      </div>
-    </Fragment>
-  );
+      </Fragment>
+    );
 }
 
 export default App;
--- app-clima/src/components/Formulario.js
+++ app-clima/src/components/Formulario.js
@@ -1,29 +1,71 @@
-import React, { Fragment } from 'react';
+import React, { useState } from 'react';
 
 //sfc funcional component
-const Formulario = () => {
+const Formulario = ({busqueda, guardarBusqueda, guardarConsulta}) => {
+
+    const [error, guardarError] = useState(false)
+    
+    // extraer ciudad y pais
+    const {ciudad, pais} = busqueda;
+
+    //funcion que coloca los elem en el state
+    const handleChange = e => {
+        //actualizar el state
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    //cuando el usuario hace submit al formulario
+    const  handleSubmit = e => {
+        e.preventDefault()
+
+        //validar los campos
+        if (ciudad.trim() === '' || pais.trim() === '') {
+            guardarError(true)
+            return;
+        }
+        guardarError(false)
+
+        //pasarlo al componente principal
+        guardarConsulta(true)
+
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            {error ? <p className="red darken-4 error">Todos los campos son obligatorios</p>: null}
            <div className="input-field col s12">
                <input
                     type="text"
                     name="ciudad"
                     id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}
                />
-                <label htmlFor="Ciudad"> Ciudad: </label>
+                <label htmlFor="ciudad">Ciudad</label>
             </div> 
 
             <div className="input-field col s12">
-                <select 
+                <input 
+                    type="text"
                     name="pais"
-                    id="pais">
-                    <option value=""> --Seleccione un pais--</option>
-                    <option value="US"> Estados Unidos</option>
-                    <option value="AR"> Argentina</option>
-                    <option value="MS"> Mexico </option>
-                    <option value="UR"> Uruguay </option>
-                </select>
-                <label htmlFor="pais">Pais: </label>
+                    id="pais"
+                    value={pais}
+                    onChange={handleChange}
+                />
+                <label htmlFor="pais">Pais</label>
+            </div>
+
+            <div className="input-field col s12">
+                <input
+                    type="submit"
+                    value="Buscar clima"
+                    className="waves-effect waves-light btn-large btn-block yellow accent-4"
+                />
             </div>
 
 
